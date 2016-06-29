3c4f0a193d0a6dfdc9ae53ef8e286015b10881ac
Joan Gamell <2460215+gamell@users.noreply.github.com>
2016-06-29 08:12:36 -0700

    Working gulp pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,123 +1,57 @@
 'use strict';
 
-const gulp = require('gulp');
 const browserSync = require('browser-sync');
-const less = require('gulp-less');
 const reload = browserSync.reload;
-const runSequence = require('run-sequence');
+const autoprefixer = require('autoprefixer');
 
-gulp.task('assets', () =>
-  gulp.src('app/less/**/*.less')
-    .pipe(less({
-      paths: ['app/less'],
-    }))
-    .pipe(plugins.sourcemaps.init())
-    .pipe(plugins.postcss([
-      require('autoprefixer-core')({browsers: ['last 1 version']})
-    ]))
-    .pipe(plugins.sourcemaps.write())
-    .pipe(gulp.dest('.tmp/styles'))
-    .pipe(reload({ stream: true }))
+// gulp modules
+
+const gulp = require('gulp');
+const less = require('gulp-less');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('html', () =>
+  gulp.src('src/*.html').pipe(gulp.dest('public'))
 );
 
 gulp.task('less', () =>
-  gulp.src('app/less/**/*.less')
+  gulp.src('src/styles/**/*.less')
     .pipe(less({
-      paths: ['app/less'],
+      paths: ['src/styles'],
     }))
-    .pipe(plugins.sourcemaps.init())
-    .pipe(plugins.postcss([
-      require('autoprefixer-core')({browsers: ['last 1 version']})
-    ]))
-    .pipe(plugins.sourcemaps.write())
-    .pipe(gulp.dest('.tmp/styles'))
+    .pipe(sourcemaps.init())
+    .pipe(postcss([autoprefixer({ browsers: ['last 1 version'] })]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('public/styles'))
     .pipe(reload({ stream: true }))
 );
 
-gulp.task('html', ['less'], () => {
-  const assets = plugins.useref.assets({ searchPath: ['.tmp', 'app', '.'] });
-  return gulp.src('app/*.html')
-    .pipe(assets)
-    .pipe(plugins.if('*.js', plugins.uglify()))
-    .pipe(plugins.if('*.css', plugins.csso()))
-    .pipe(assets.restore())
-    .pipe(plugins.useref())
-    .pipe(plugins.if('*.html', plugins.minifyHtml({ conditionals: true, loose: true })))
-    .pipe(gulp.dest('dist'));
-});
+gulp.task('build', ['html', 'less']);
 
-gulp.task('images', () =>
-  gulp.src('app/images/**/*')
-    .pipe(plugins.cache(plugins.imagemin({
-      progressive: true,
-      interlaced: true,
-      // don't remove IDs from SVGs, they are often used
-      // as hooks for embedding and styling
-      svgoPlugins: [{ cleanupIDs: false }]
-    })))
-    .pipe(gulp.dest('dist/images'))
-);
-
-gulp.task('fonts', () =>
-  gulp.src(require('main-bower-files')({
-    filter: '**/*.{eot,svg,ttf,woff,woff2}'
-  }).concat('app/fonts/**/*'))
-    .pipe(gulp.dest('.tmp/fonts'))
-    .pipe(gulp.dest('dist/fonts'))
-);
-
-gulp.task('extras', () =>
-  gulp.src([
-    'app/*.*',
-    '!app/*.html',
-  ], {
-    dot: true,
-  }).pipe(gulp.dest('dist'))
-);
-
-gulp.task('clean', require('del').bind(null, ['.tmp/**/*', 'dist/**/*']));
-
-gulp.task('serve', ['less', 'fonts'], () => {
+gulp.task('serve', ['clean', 'build'], () => {
   browserSync({
     notify: false,
     port: 9000,
     server: {
-      baseDir: ['.tmp', 'app'],
-      routes: {
-        '/bower_components': 'bower_components',
-      },
+      baseDir: ['public'],
     },
   });
 
   // watch for changes
   gulp.watch([
-    'app/*.html',
-    'app/scripts/**/*.js',
-    'app/images/**/*',
-    'app/less/**/*',
-    '.tmp/fonts/**/*',
+    'src/*.html',
+    'src/scripts/**/*.js',
+    'src/images/**/*',
+    'src/styles/**/*',
+    'public/fonts/**/*',
   ]).on('change', reload);
 
-  gulp.watch('app/less/**/*.less', ['less']);
-  gulp.watch('app/fonts/**/*', ['fonts']);
+  gulp.watch('src/styles/**/*.less', ['less']);
+  gulp.watch('src/*.html', ['html']);
+  // gulp.watch('src/fonts/**/*', ['fonts']);
 });
 
-gulp.task('build', ['html', 'less', 'fonts', 'images', 'extras'], () =>
-  gulp.src('dist/**/*').pipe(plugins.size({ title: 'build', gzip: true }))
-);
+gulp.task('clean', require('del').bind(null, ['public/**/*']));
 
-gulp.task('copy', () => {
-  gulp.src('dist/**/*')
-  .pipe(gulp.dest('../../public'));
-});
-
-gulp.task('prepare-visualizer', (cb) => {
-  runSequence('clean',
-              'build',
-              'copy',
-              cb);
-});
-
-gulp.task('default', ['clean'], () => {
-  gulp.start('build');
-});
+gulp.task('default', ['serve']);
